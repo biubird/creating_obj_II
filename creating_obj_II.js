6708780d2a9ef8221925508505575cb9ec8e2c3b
Heather Doucet <heatherredoucet@gmail.com>
2016-09-13 08:38:56 -0500

    final

--- creating_obj_II.js
+++ creating_obj_II.js
@@ -1,34 +1,67 @@
-function VehicleConstructor(name, num_wheels, num_passengers){
+// Modify your VehicleConstructor (from earlier) to use this & new (and follow the JS conventions talked about in the previous chapters).
 
-  var vehicle = {};
+// Then make the following modifications:
+//
+// Have the Vehicle constructor also take in a “speed”
+// Store the speed as an attribute
+// Create a private variable called distance_travelled that starts at 0
+// Create a private method called updateDistanceTravelled that increments distance travelled by speed
+// Add a method to the Vehicle called “move” that calls updateDistanceTravelled and then makeNoise
+// Add a method called checkMiles that console.logs the distance_travelled
 
-    vehicle.name = name;
-    vehicle.num_wheels = num_wheels;
-    vehicle.num_passengers = num_passengers;
 
-    vehicle.make_noise = function(){
-      console.log("Make some noise!");
+function VehicleConstructor(name, num_wheels, num_passengers, speed){
+  if (!(this instanceof VehicleConstructor)){
+    return new VehicleConstructor(name, num_wheels, num_passengers, speed)
+  }
+    var distance_travelled = 0;
+    var self = this;
+    function updateDistanceTravelled() {
+      return (distance_travelled, self.speed);
+      console.log(distance_travelled);
     }
-    return vehicle;
+//  var vehicle = {};
+
+    this.name = name;
+    this.num_wheels = num_wheels;
+    this.num_passengers = num_passengers;
+    this.speed = speed;
+    this.move = function(){
+      updateDistanceTravelled();
+      this.make_noise();
+    };
+    this.checkMiles = function() {
+      console.log(distance_travelled);
+    };
+    this.make_noise = function(){
+      console.log("Make some noise!");
+    };
+  //  return vehicle;
   }
 
-  var buick = VehicleConstructor('buick', 4, 1);
+  var buick = new VehicleConstructor('buick', 4, 1, 40);
   buick.make_noise = function(){
     console.log(buick.name + " goes " + "BEEP BEEP");
   }
-  var bike = VehicleConstructor('bike', 2, 1);
+  var bike = new VehicleConstructor('bike', 2, 1, 15);
   bike.make_noise = function(){
     console.log(bike.name + " goes " + "RING RING");
   }
-  var sedan = VehicleConstructor('sedan', 4, 4);
+  var sedan = new VehicleConstructor('sedan', 4, 4, 65);
   sedan.make_noise = function(){
     console.log(sedan.name + " goes " + "HONK HONK");
   }
-  var bus = VehicleConstructor('bus', 6, 12);
+  var bus = new VehicleConstructor('bus', 6, 12, 35);
 
   bus.passenger_pickup = function(new_passengers){
   bus.num_passengers += new_passengers;
   }
 
-bus.passenger_pickup(10);
-console.log(bus.num_passengers);
+ bus.passenger_pickup(10);
+ console.log(bus.num_passengers);
+
+console.log(buick.num_wheels);
+console.log(buick.speed);
+buick.move();
+buick.checkMiles();
+buick.make_noise();
